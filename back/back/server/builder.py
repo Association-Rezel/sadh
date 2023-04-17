0dba35b8834430918d968ecd9f2a53ed2fbe6b8a
Thibaud Labat <thibaudlabat@gitlab.com>
2023-04-17 23:50:58 +0200

    front: admin / liste adhérents

--- back/back/server/builder.py
+++ back/back/server/builder.py
@@ -1,6 +1,11 @@
 """Build the server."""
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
 
 
 from back.server.user import router as user_router
@@ -13,8 +18,20 @@ def root():
 def build() -> FastAPI:
     """Build the app from interfaces."""
     app = FastAPI()
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.get("/")(root)
     app.include_router(user_router)
+
+
+
 
     return app
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,22 +1,22 @@
-import './App.css'
+import { useState, useEffect } from 'react';
 import {BrowserRouter, Routes, Route, Outlet, Link} from "react-router-dom";
 import {Button} from "@mui/material";
 
-function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="admin" element={<Admin />} />
-                <Route path="abonne" element={<Abonne />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  )
-}
+import './App.css'
 
-export default App
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="admin" element={<Admin />} />
+                    <Route path="abonne" element={<Abonne />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
 
 function Layout() {
     return (
@@ -43,8 +43,35 @@ function Home() {
 }
 
 function Admin() {
+
+    const [users, setUsers] = useState([])
+    const fetchUsers = async () => {
+        const response = await fetch("http://localhost:8000/users")
+        const users = await response.json()
+        console.log(users)
+        setUsers(users)
+    }
+
+    useEffect(()=>{
+        fetchUsers()
+    },[])
+
+
+
     return (
-        <h1>Admin</h1>
+        <div>
+            <h1>Admin</h1>
+            <p> NB : Il faut lancer le backend sur localhost:8000 pour faire marcher ce code.</p>
+
+            <div className="card">
+                <h2>Users</h2>
+                {
+                    users && users.length>0 && users.map((item:any)=><p>{item.id}</p>)
+                }
+
+            </div>
+
+        </div>
     )
 }
 function Abonne() {
@@ -52,3 +79,21 @@ function Abonne() {
         <h1>Espace abonné</h1>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
