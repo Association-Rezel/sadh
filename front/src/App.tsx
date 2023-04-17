48fbc1f82f4949b04fd45cfc3ecffa038a307575
Thibaud Labat <thibaudlabat@gitlab.com>
2023-04-18 00:53:08 +0200

    front: barre menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,21 @@
-import {BrowserRouter, Routes, Route, Outlet, Link} from "react-router-dom";
-import {Button} from "@mui/material";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 
 import {PageAdmin} from "./pages/PageAdmin";
 import {PageAdherent} from "./pages/PageAdherent";
 import {PageAccueil} from "./pages/PageAccueil";
 
 import './App.css'
+import {MenuBar} from "./components/MenuBar";
+import * as React from "react";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<PageAccueil />} />
-                    <Route path="admin" element={<PageAdmin />} />
-                    <Route path="adherents" element={<PageAdherent />} />
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<PageAccueil/>}/>
+                    <Route path="admin" element={<PageAdmin/>}/>
+                    <Route path="adherents" element={<PageAdherent/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
@@ -24,25 +25,17 @@ function App() {
 function Layout() {
     return (
         <>
-            <Button variant="outlined">
-                <Link to="/">Home</Link>
-            </Button>
-            <Button variant="outlined">
-                <Link to="/admin">Admin</Link>
-            </Button>
-            <Button variant="outlined">
-                <Link to="/adherents">Espace adhérents</Link>
-            </Button>
+            <MenuBar pages={[
+                {titre: "Accueil", route: "/"},
+                {titre: "Espace adhérents", route: "/adherents"},
+                {titre: "Espace Administrateurs", route: "/admin"},
+            ]}/>
 
             <Outlet/>
         </>
     )
 }
 
+export default App;
 
 
-
-
-
-
-export default App
