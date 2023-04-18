2cd358cfc80aa13d52c1211ec2e98619b423dae5
QuentinN42 <quentin@lieumont.fr>
2023-04-18 18:53:50 +0200

    fix: redirectUris fixed
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- infra/seed-kc.sh
+++ infra/seed-kc.sh
@@ -17,5 +17,5 @@ while ! curl -fsL localhost:8080 -w "%{http_code}\n" -o/dev/null; do sleep 1; do
 cd /opt/jboss/keycloak/bin/
 ./kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password admin
 ./kcadm.sh create realms -s realm=users -s enabled=true -s registrationAllowed=true -s registrationEmailAsUsername=true
-./kcadm.sh create clients -r users -s clientId=back -s standardFlowEnabled=true -s publicClient=false -s clientAuthenticatorType=client-secret -s secret=d0b8122f-8dfb-46b7-b68a-f5cc4e25d000 -s directAccessGrantsEnabled=true
+./kcadm.sh create clients -r users -s clientId=back -s standardFlowEnabled=true -s publicClient=false -s clientAuthenticatorType=client-secret -s secret=d0b8122f-8dfb-46b7-b68a-f5cc4e25d000 -s directAccessGrantsEnabled=true -s redirectUris="[\"http://localhost:5173/*\"]"
 EOS'
