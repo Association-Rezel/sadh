b617b0cb63688bbf2d0bc44790144926bb786ac4
QuentinN42 <quentin@lieumont.fr>
2023-05-09 12:45:04 +0200

    fix: lint auth.py
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/back/server/auth.py
+++ back/back/server/auth.py
@@ -2,15 +2,17 @@
 
 # https://github.com/tiangolo/fastapi/discussions/9066
 
-from fastapi import APIRouter, status, Depends, HTTPException, Security
+from fastapi import APIRouter, Depends, HTTPException, Security, status
+from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
 from pydantic import Json
 
-from back.core.auth import keycloak_openid, KEYCLOAK_PUBLIC_KEY, oauth2_scheme
+from back.core.auth import KEYCLOAK_PUBLIC_KEY, keycloak_openid, oauth2_scheme
 
 router = APIRouter(prefix="/auth", tags=["users"])
 
 
 async def get_auth(token: str = Security(oauth2_scheme)) -> Json:
+    """Get the user's identity from the token or terminate the request."""
     try:
         return keycloak_openid.decode_token(
             token,
@@ -18,23 +20,26 @@ async def get_auth(token: str = Security(oauth2_scheme)) -> Json:
             options={
                 "verify_signature": True,
                 "verify_aud": False,  # TODO : verify audience (mettre sur True et corriger les erreurs)
-                "exp": True
-            }
+                "exp": True,
+            },
         )
-    except Exception as e:
+    except (JWTError, ExpiredSignatureError, JWTClaimsError) as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=str(e),  # "Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
-        )
+        ) from e
 
 
 @router.get("/user")
 async def get_current_user(
-        identity: Json = Depends(get_auth)
+    identity: Json = Depends(get_auth),
 ):
-    # TODO : Renvoyer les informations utiles à l'utilisateur ; cf le type "User" du front
+    """Get the current user's identity.
+
+    TODO : Renvoyer les informations utiles à l'utilisateur ; cf le type "User" du front
+    """
     return {
         "id": identity["sub"],
-        "isAdmin": True
+        "isAdmin": True,
     }
