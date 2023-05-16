087b8867824067867735c8258701441e8bf8b975
QuentinN42 <quentin@lieumont.fr>
2023-05-16 17:33:59 +0200

    refactor: moved code and add user name
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/back/core/users.py
+++ back/back/core/users.py
@@ -1,11 +1,31 @@
 """User CRUDs."""
+import logging
+
 from sqlalchemy.orm import Session
 
 from back.database import models
 from back.interfaces import User
+from back.keycloak_client import Token
+
+__logger = logging.getLogger(__name__)
 
 
 def get_users(db: Session) -> list[User]:
     """Get all users."""
-    users = db.query(models.User).all()
-    return [User(keycloak_id=user.keycloak_id) for user in users]
+    return list(
+        map(
+            User.from_orm,
+            db.query(models.User).all(),
+        ),
+    )
+
+
+def get_or_create_from_token(db: Session, token: Token) -> User:
+    """Decode JWT and find user in the database or create it."""
+    u = db.get(models.User, token.sub)
+    if not u:
+        __logger.info("Creating user %s", token.preferred_username)
+        u = models.User(keycloak_id=token.sub, name=token.preferred_username)
+        db.add(u)
+        db.commit()
+    return User.from_orm(u)
--- back/back/server/user.py
+++ back/back/server/user.py
@@ -3,6 +3,7 @@ import logging
 
 from fastapi import APIRouter, Depends
 
+from back.core.users import get_or_create_from_token, get_users
 from back.database import Session, get_db
 from back.database.users import User as DBUser
 from back.interfaces import User
@@ -13,15 +14,9 @@ __logger = logging.getLogger(__name__)
 
 
 @Depends
-def user_from_jwt(db: Session = Depends(get_db), token: Token = protect) -> DBUser:
+def user_from_jwt(db: Session = Depends(get_db), token: Token = protect) -> User:
     """Decode JWT and find user in the database or create it."""
-    u = db.get(DBUser, token.sub)
-    if not u:
-        __logger.info("Creating user %s", token.sub)
-        u = DBUser(keycloak_id=token.sub)
-        db.add(u)
-        db.commit()
-    return u
+    return get_or_create_from_token(db, token)
 
 
 @Depends
@@ -32,14 +27,14 @@ def must_be_admin(user: DBUser = user_from_jwt) -> None:
 
 
 @router.get("/")
-def get_users(db: Session = Depends(get_db), _: None = must_be_admin) -> list[User]:
+def _(db: Session = Depends(get_db), _: None = must_be_admin) -> list[User]:
     """This is some docs."""
-    return [User(keycloak_id=u.keycloak_id, is_admin=u.is_admin) for u in db.query(DBUser).all()]
+    return get_users(db)
 
 
 @router.get("/me")
-async def get_current_user(
-    user: DBUser = user_from_jwt,
+async def _me(
+    user: User = user_from_jwt,
 ) -> User:
     """Get the current user's identity."""
-    return User(keycloak_id=user.keycloak_id, is_admin=user.is_admin)
+    return user
