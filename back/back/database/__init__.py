ea8b4988c67f96733522af86db8d4445c67708ee
QuentinN42 <quentin@lieumont.fr>
2023-05-17 10:27:22 +0200

    docs: add docstring
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/back/database/__init__.py
+++ back/back/database/__init__.py
@@ -1,5 +1,5 @@
 """All database models are defined here."""
 from back.database import models
-from back.database.main import Base, Session, engine, get_db
+from back.database.main import Base, Session, SessionLocal, engine
 
 Base.metadata.create_all(bind=engine)
--- back/back/middlewares/__init__.py
+++ back/back/middlewares/__init__.py
@@ -1,1 +1,3 @@
 """API middlewares."""
+from back.middlewares.auth import token
+from back.middlewares.db import db, must_be_admin, user
