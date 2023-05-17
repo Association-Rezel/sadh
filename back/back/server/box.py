5ec4dc31cc1761627aea50052b5dfdaf9b103c0a
QuentinN42 <quentin@lieumont.fr>
2023-05-17 14:04:38 +0200

    feat: lease and port
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/back/server/box.py
+++ back/back/server/box.py
@@ -1,23 +1,24 @@
 """Gestion d'une box."""
+
 from fastapi import APIRouter
 
-from back.interfaces.box import IPAddresses46
-from back.netbox_client.models import Adherent, Box, BoxModel, Chambre, Residence
+from back.netbox_client.models import (
+    Adherent,
+    Box,
+    BoxModel,
+    Chambre,
+    DHCPLease,
+    IPv4,
+    PortBinding,
+    Protocols,
+    Residence,
+)
 
 router = APIRouter(prefix="/box", tags=["box"])
 
 
-@router.get("/ip")
-def get_ip() -> IPAddresses46:
-    """Tmp."""
-    return IPAddresses46(ipv4="127.0.0.1", ipv6="::1")
-
-
 # TODO: implements
-# - Box
-# - DHCPLease
-# - Device
-# - PortRule
+# - Conneced devices
 
 
 @router.get("/")
@@ -31,5 +32,30 @@ def _() -> Box:
             name="404",
             adherent=Adherent("123"),
         ),
+        adherent=Adherent("123"),
+    )
+
+
+@router.get("/lease")
+def _lease() -> DHCPLease:
+    """Return the current user box."""
+    return DHCPLease(
+        ip=IPv4("192.168.1.1"),
+        mac="00:00:00:00:00:00",
+        hostname="pc1.local",
+        adherent=Adherent("123"),
+    )
+
+
+@router.get("/port")
+def _port() -> PortBinding:
+    """Return the current user box."""
+    return PortBinding(
+        name="Minecraft",
+        ext_ip=IPv4("1.1.1.1/0"),
+        ext_port=25565,
+        int_ip=IPv4("192.168.1.42/24"),
+        int_port=25575,
+        proto=Protocols.TCP,
         adherent=Adherent("123"),
     )
