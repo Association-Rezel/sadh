195af1e28e35bf008579bae16d21df2b18ad9d4a
Severin Messiaen <messiaen.severin@gmail.com>
2023-08-27 12:01:56 +0000

    feat: Add basic user lookup page for admins
    See merge request rezel/faipp/site/site!38

---
+++ front/src/components/Users/User.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ONT, Status, Subscription, User as UserType } from "../../utils/types";
+import { Api } from "../../utils/Api";
+import { Typography } from "@mui/material";
+
+function User() {
+    let { keycloak_id } = useParams();
+
+    const [user, setUser] = useState<UserType>({ name: "", email: "", keycloak_id: "", is_admin: false });
+    const [ont, setONT] = useState<ONT>();
+    const [subscription, setSubscription] = useState<Subscription>();
+
+    useEffect(() => {
+        Api.fetchUser(keycloak_id).then(user => {
+            setUser(user);
+        });
+
+        Api.fetchONT(keycloak_id).then(ont => {
+            setONT(ont);
+        });
+
+        Api.fetchSubscription(keycloak_id).then(subscription => {
+            setSubscription(subscription);
+        });
+    }, [keycloak_id]);
+
+    return (
+        <div className="card">
+            <UserSection user={user} />
+            <SubscriptionSection subscription={subscription} />
+            <ONTSection ont={ont} />
+        </div>
+    );
+};
+
+function UserSection({ user }: { user: UserType }) {
+    if (!user) return (<></>);
+
+    return (
+        <>
+            <Typography variant="h3" align="center" color="text.primary" component="div" sx={{ marginTop: 3 }}>
+                Adhérent : {user.name}
+            </Typography>
+            <Typography variant="body1" align="center" color="text.secondary" component="div" sx={{ marginTop: 3 }}>
+                <strong>{user.email}</strong>
+            </Typography>
+        </>
+    )
+}
+
+function SubscriptionSection({ subscription }: { subscription: Subscription }) {
+    if (!subscription) return (<></>);
+
+    return (
+        <>
+            <Typography variant="h5" align="left" color="text.primary" component="div" sx={{ marginTop: 10 }}>
+                Adhésion <Typography fontSize={10}>{subscription.subscription_id}</Typography>
+            </Typography>
+            <Typography variant="body1" align="left" color="text.secondary" component="div" sx={{ marginTop: 3 }}>
+                <strong>Statut</strong> : {Status[subscription.status]}<br />
+                <strong>Chambre</strong> : {subscription.chambre.name} - {subscription.chambre.residence}<br />
+            </Typography>
+        </>
+    )
+}
+
+function ONTSection({ ont }: { ont: ONT }) {
+    if (!ont) return (<></>);
+
+    return (
+        <>
+            <Typography variant="body1" align="left" color="text.secondary" component="div" sx={{ marginTop: 10 }}>
+                <strong>Numéro de série</strong> : {Status[ont.serial]}<br />
+                <strong>Position au PM</strong> : {Status[ont.position_PM]}<br />
+            </Typography>
+        </>
+    )
+}
+
+export default User;
