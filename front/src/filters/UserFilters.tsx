d62b390340cb568fb1ea09ea67d3e7134f45e288
Séverin Messiaen <messiaen.severin@gmail.com>
2023-09-12 20:08:29 +0200

    feat: dashboards

--- front/src/filters/UserFilters.tsx
+++ front/src/filters/UserFilters.tsx
@@ -1,40 +1,40 @@
 import { Residence, SubscriptionStatus, UserDataBundle } from "../utils/types";
 
-export interface UserFilter{
-    filter(Users: UserDataBundle[]): UserDataBundle[];
+export interface UserFilter {
+    filter(user: UserDataBundle): boolean;
 }
 
-export class ResidencesFilter implements UserFilter{
+export class ResidencesFilter implements UserFilter {
     private residence: Residence;
-    
-    constructor(residence: Residence){
+
+    constructor(residence: Residence) {
         this.residence = residence;
     }
 
-    filter(Users: UserDataBundle[]): UserDataBundle[]{
-        return Users.filter((User) => User.subscription?.chambre.residence === this.residence);
+    filter(user: UserDataBundle): boolean {
+        return user.subscription?.chambre.residence === this.residence;
     }
 }
 
-export class StatusFilter implements UserFilter{
+export class StatusFilter implements UserFilter {
     private status: SubscriptionStatus;
-    
-    constructor(status: SubscriptionStatus){
+
+    constructor(status: SubscriptionStatus) {
         this.status = status;
     }
 
-    filter(Users: UserDataBundle[]): UserDataBundle[]{
-        return Users.filter((User) => User.subscription?.status === this.status);
+    filter(user: UserDataBundle): boolean {
+        return user.subscription?.status === this.status;
     }
 }
 
-export function filterUsers(users: UserDataBundle[], filters: UserFilter[]): UserDataBundle[]{
+export function filterUsers(users: UserDataBundle[], filters: UserFilter[]): UserDataBundle[] {
     let filteredUsers = users;
     console.log("filters", filters);
     console.log("filteredUsers", filteredUsers);
     filters.forEach((filter) => {
         console.log("filteredUsers", filteredUsers);
-        filteredUsers = filter.filter(filteredUsers);
+        filteredUsers = filteredUsers.filter((user) => filter.filter(user));
     });
     return filteredUsers;
 }
--- front/src/pages/admin/PageAdmin.tsx
+++ front/src/pages/admin/PageAdmin.tsx
@@ -1,13 +1,13 @@
-import React, {useContext} from "react";
+import {useContext} from "react";
 import {AppStateContext} from "../../utils/AppState";
+import Dashboards from "../../components/AdminDashboard/Dashboard/Dashboards";
 
 export function PageAdmin() {
     const appState = useContext(AppStateContext);
     return (
         <div>
-            <h1>Admin</h1>
-            {JSON.stringify(appState)}
-            <p> NB : Il faut lancer le backend sur localhost:8000 pour faire marcher ce code.</p>
+            <h1>Dashboards Admin</h1>
+            <Dashboards />
         </div>
     );
 }
