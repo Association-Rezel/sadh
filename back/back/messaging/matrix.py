e49893645b0d46b6ec5d5efab72490a088c430a9
Séverin Messiaen <messiaen.severin@gmail.com>
2024-08-11 19:34:35 +0200

    fix: thread safety

--- back/back/messaging/matrix.py
+++ back/back/messaging/matrix.py
@@ -7,27 +7,13 @@ from nio import AsyncClient
 from back.env import ENV
 
 
-class MatrixClientFactory:
-    client: AsyncClient
-
-    async def create_client(self):
-        self.client = AsyncClient("https://matrix.rezel.net", ENV.matrix_user)
-        await self.client.login(ENV.matrix_password)
-
-    async def get_client(self):
-        if not hasattr(self, "client"):
-            await self.create_client()
-
-        return self.client
-
-
 def send_matrix_message(*lines: str) -> None:
     threading.Thread(target=asyncio.run, args=(async_send_matrix_message(*lines),)).start()
 
 
 async def async_send_matrix_message(*lines: str) -> None:
-    matrix_client = MatrixClientFactory()
-    client = await matrix_client.get_client()
+    client = AsyncClient("https://matrix.rezel.net", ENV.matrix_user)
+    await client.login(ENV.matrix_password)
 
     await client.room_send(
         room_id=ENV.matrix_room,
