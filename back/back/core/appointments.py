e82b1fc0ef14952445ede30ee3116479e4f5e3ea
Séverin Messiaen <messiaen.severin@gmail.com>
2024-09-06 12:09:10 +0200

    fix: appointment selection drift due to timezone

--- back/back/core/appointments.py
+++ back/back/core/appointments.py
@@ -4,26 +4,27 @@ from back.mongodb.user_models import AppointmentSlot
 
 
 def get_week_appointment_slots(
-    date: datetime, weekOffset: int
+    date: datetime, week_offset: int
 ) -> list[AppointmentSlot]:
     """Get all available slots in the week of the given date."""
     # For the moment slots are hardcoded
 
-    startOfWeek = date - timedelta(days=date.weekday())
-    startOfWeek = startOfWeek.replace(hour=0, minute=0, second=0, microsecond=0)
+    start_of_week = date - timedelta(days=date.weekday())
+    start_of_week = start_of_week.replace(hour=0, minute=0, second=0, microsecond=0)
 
-    hourSlots = [(8, 11), (10, 13), (13, 16), (15, 18)]
+    hour_slots = [(8, 11), (10, 13), (13, 16), (15, 18)]
+    timezone_delta = 2
 
-    startOfWeek = date - timedelta(days=date.weekday())
-    startOfWeek = startOfWeek.replace(hour=0, minute=0, second=0, microsecond=0)
-    startOfWeek += timedelta(days=weekOffset * 7)
+    start_of_week = date - timedelta(days=date.weekday())
+    start_of_week = start_of_week.replace(hour=0, minute=0, second=0, microsecond=0)
+    start_of_week += timedelta(days=week_offset * 7)
 
     slots: list[AppointmentSlot] = []
     for i in range(0, 6):
-        day = startOfWeek + timedelta(days=i)
-        for slot in hourSlots:
-            start = day.replace(hour=slot[0])
-            end = day.replace(hour=slot[1])
+        day = start_of_week + timedelta(days=i)
+        for slot in hour_slots:
+            start = day.replace(hour=slot[0] - timezone_delta)
+            end = day.replace(hour=slot[1] - timezone_delta)
             # Check that the day is at least D+8
             if start > datetime.now() + timedelta(days=8):
                 slots.append(AppointmentSlot(start=start, end=end))
