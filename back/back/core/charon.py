1c635fc8e5ffc3b616e7b3ba0cd798e6d4f24744
Séverin Messiaen <messiaen.severin@gmail.com>
2024-09-07 13:40:13 +0200

    feat: can unregister ONT & box

--- back/back/core/charon.py
+++ back/back/core/charon.py
@@ -6,11 +6,33 @@ from back.messaging.matrix import send_matrix_message
 
 def register_ont_in_olt(ont_serial: str) -> None:
     """Register an ONT in the OLT."""
+    if ENV.deploy_env is not "prod":
+        print(f"ENV IS NOT PROD - Registering ONT {ont_serial} in OLT")
+        return
+
     r = requests.get(f"{ENV.charon_url}/register-onu/{ont_serial}", timeout=5)
 
     if r.status_code != 200:
         send_matrix_message(
             f"❌ Erreur lors de l'enregistrement de l'ONT {ont_serial} dans l'OLT",
+            "```",
+            f"Code {r.status_code}",
+            r.text,
+            "```",
+        )
+
+
+def unregister_ont_in_olt(ont_serial: str) -> None:
+    """Unregister an ONT in the OLT."""
+    if ENV.deploy_env is not "prod":
+        print(f"ENV IS NOT PROD - Unregistering ONT {ont_serial} in OLT")
+        return
+
+    r = requests.get(f"{ENV.charon_url}/unregister-onu/{ont_serial}", timeout=5)
+
+    if r.status_code != 200:
+        send_matrix_message(
+            f"❌ Erreur lors de la suppression de l'ONT {ont_serial} dans l'OLT",
             "```",
             f"Code {r.status_code}",
             r.text,
