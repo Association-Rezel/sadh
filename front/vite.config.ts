1c422f22b9b55a7b12404722c67cc1ed68dfe7de
Baptiste Drouillet <baptiste@rezel.net>
2025-06-22 17:44:18 +0000

    feat: enhance TableUsers component with sorting and filtering capabilities

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+    if (mode === 'dev') {
+        return {
+            plugins: [react()],
+            server: {
+                watch: {
+                    usePolling: true,
+                    interval: 100,
+                },
+            },
+        }
+    }
     return {
         plugins: [react()],
     }
